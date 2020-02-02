69da21b589b33cf06738897216af501dcc3fdd42
kaconk <harutonouciha@gmail.com>
2020-02-02 17:36:09 +0700

    datetimepicker

---
+++ lib/model/UserModel.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class User {
+  final TextEditingController EmailUser = new TextEditingController();
+  final TextEditingController NamaUser = new TextEditingController();
+  final TextEditingController PassUser = new TextEditingController();
+}
--- lib/view/form.dart
+++ lib/view/form.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:form/model/UserModel.dart';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
 class FormRegister extends StatefulWidget {
   _FormRegisterState createState() => _FormRegisterState();
 }
 
 class _FormRegisterState extends State<FormRegister> {
+  User _user;
+  final dateFormat = DateFormat("yyyy-MM-dd");
   @override
   List<String> lvl_user = ["User", "Manager", "Administrator"];
   String _level = "User";
+  String _kelamin = "";
+
+  void _pilihjk(String value) {
+    setState(() {
+      _kelamin = value;
+    });
+  }
 
   void pilih_level(String value) {
     setState(() {
@@ -31,12 +43,14 @@ class _FormRegisterState extends State<FormRegister> {
               child: new Column(
                 children: <Widget>[
                   TextField(
+                      //controller: _user.EmailUser,
                       decoration: InputDecoration(
                           border: OutlineInputBorder(), labelText: "Email")),
                   new Padding(
                     padding: EdgeInsets.only(top: 20.0),
                   ),
                   TextField(
+                    //controller: _user.NamaUser,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(), labelText: "Nama"),
                   ),
@@ -44,6 +58,7 @@ class _FormRegisterState extends State<FormRegister> {
                     padding: EdgeInsets.only(top: 20.0),
                   ),
                   TextField(
+                    //controller: _user.PassUser,
                     obscureText: true,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(), labelText: "Password"),
@@ -54,7 +69,51 @@ class _FormRegisterState extends State<FormRegister> {
                   new Row(
                     children: <Widget>[
                       new Text(
-                        "Level User     ",
+                        "Jenis Kelamin :    ",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                  new RadioListTile(
+                    value: "L",
+                    title: Text("Laki-Laki"),
+                    groupValue: _kelamin,
+                    onChanged: (String value) {
+                      _pilihjk(value);
+                    },
+                    activeColor: Colors.blue,
+                  ),
+                  new RadioListTile(
+                    value: "P",
+                    title: Text("Perempuan"),
+                    groupValue: _kelamin,
+                    onChanged: (String value) {
+                      _pilihjk(value);
+                    },
+                    activeColor: Colors.blue,
+                  ),
+                  new Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text('Basic date field (${dateFormat.pattern})'),
+                    ],
+                  ),
+                  DateTimeField(
+                    format: dateFormat,
+                    onShowPicker: (context, currentValue) async {
+                      return showDatePicker(
+                          context: context,
+                          firstDate: DateTime(1900),
+                          initialDate: currentValue ?? DateTime.now(),
+                          lastDate: DateTime(2100));
+                    },
+                  ),
+                  new Row(
+                    children: <Widget>[
+                      new Text(
+                        "Level User :    ",
                         style: TextStyle(fontSize: 15),
                       ),
                       new DropdownButton(
@@ -70,17 +129,17 @@ class _FormRegisterState extends State<FormRegister> {
                           return new DropdownMenuItem(
                               value: value, child: new Text(value));
                         }).toList(),
-                      )
+                      ),
                     ],
                   ),
                   new Padding(
                     padding: EdgeInsets.only(top: 20.0),
                   ),
-                  new Row(
+                  Center(
+                      child: Container(
+                          child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      new Padding(
-                        padding: EdgeInsets.only(left: 80),
-                      ),
                       new RaisedButton(
                         child: new Text(
                           "Simpan",
@@ -101,11 +160,11 @@ class _FormRegisterState extends State<FormRegister> {
                         ),
                         color: Colors.blue,
                         onPressed: () {
-                          _get_user();
+                          Navigator.pop(context);
                         },
                       ),
                     ],
-                  )
+                  )))
                 ],
               ),
             ),
